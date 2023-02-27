a0cd35627e33495c3498bd0aa6257b211e0ad80e
nikachu404 <yasinskaa.veronika@gmail.com>
2023-02-27 20:14:32 +0200

    cleared comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import './App.scss';
-// import { Route, Routes } from 'react-router-dom';
-// import { HomePage } from './components/HomePage/HomePage';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
-// import { PhonesPage } from './components/PhonesPage/PhonesPage';
-// import { TabletsPage } from './components/TabletsPage/TabletsPage';
-// import { AccessoriesPage } from './components/AccessoriesPage/AccessoriesPage';
-// import { PageNotFound } from './components/PageNotFound/PageNotFound';
-
-// interface Props {
-//   onClick: () => void;
-// }
-
-// export const Provider: React.FC<Props> = React.memo(
-//   ({ onClick, children }) => (
-//     <button
-//       type="button"
-//       onClick={onClick}
-//     >
-//       {children}
 
 export const App: React.FC = () => {
   return (
