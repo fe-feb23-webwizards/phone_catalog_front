f412fd335de0677bcf52a7ee4cc2965095e31dd1
Anton Martynes <martynes1997@gmail.com>
2023-02-27 15:20:13 +0200

    eslint check

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,7 @@ export const App: React.FC = () => {
     <div className="starter">
       <Provider onClick={() => ({})}>
         <h1>Hello Web Wizards</h1>
-      </Provider
-        />
+      </Provider sad>
     </div>
   );
 };
