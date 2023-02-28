391b93dd92f0fe3d4cc30db3e6ef8e616ebb4d83
Andrii Petrinko <apetrinko@gmail.com>
2023-02-28 16:53:09 +0200

    create slider for products cards

--- src/components/NewModelsList/NewModelsList.tsx
+++ src/components/NewModelsList/NewModelsList.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewModelsList.scss';
 import heart from '../../stylesScss/images/heart.svg';
 import phones from '../../data/phones.json';
 
 export const NewModelsList = () => {
-  const newModelsPhones = phones.sort((a, b) => b.year - a.year);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  newModelsPhones.length = 4;
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === phones.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? phones.length - 1 : currentSlide - 1);
+  };
+
+  const slidesToShow = phones.slice(currentSlide, currentSlide + 4);
 
   return (
-    <section>
-      <h1 className="title">Brand new models</h1>
+    <section className="newModels">
+      <h1 className="newModels__title">Brand new models</h1>
+      <div className="newModels__slider-control">
+        <button type="button" className="slider-control__button" onClick={prevSlide}>
+          Prev
+        </button>
+        <button type="button" className="slider-control__button" onClick={nextSlide}>
+          Next
+        </button>
+      </div>
       <div className="catalog">
-        {newModelsPhones.map(el => (
+        {slidesToShow.map(el => (
           <div className="card" key={el.id}>
-
-            <img
-              src={el.image}
-              alt="iphone"
-              className="card__img"
-            />
-            <h2 className="card__name">
-              {el.name}
-            </h2>
-
+            <img src={el.image} alt="iphone" className="card__img" />
+            <h2 className="card__name">{el.name}</h2>
             <span className="card__price">{`${el.price}$`}</span>
-
             <div className="card__underline"></div>
-
             <div className="card__description">
               <span className="description__title">Screen</span>
               <span className="description__value">{el.screen}</span>
@@ -40,23 +46,14 @@ export const NewModelsList = () => {
               <span className="description__title">RAM</span>
               <span className="description__value">{el.ram}</span>
             </div>
-
             <div className="card__flex">
-              <a
-                href="/"
-                className="card__button"
-              >
+              <a href="/" className="card__button">
                 Add to cart
               </a>
-
-              <button
-                type="button"
-                className="card__add-to-favorite"
-              >
+              <button type="button" className="card__add-to-favorite">
                 <img src={heart} alt="favorite" />
               </button>
             </div>
-
           </div>
         ))}
       </div>
