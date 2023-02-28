7bae1a43d88428ebd1626078ebca3b5d5c88d391
Anton Martynes <martynes1997@gmail.com>
2023-02-28 16:56:29 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import React, { memo, useCallback } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../../stylesScss/images/FooterLogo.svg';
 
 import './Footer.scss';
 
-const footerNav = [
+const footerLinks = [
   {
-    title: 'github',
-    path: '/github',
+    link: 'GITHUB',
+    url: 'https://github.com/fe-feb23-webwizards?type=source',
   },
   {
-    title: 'contacts',
-    path: '/contacts',
+    link: 'CONTACTS',
+    url: 'https://github.com/orgs/fe-feb23-webwizards/people',
   },
   {
-    title: 'rights',
-    path: '/rights',
+    link: 'RIGHTS',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Copyright-_all_rights_reserved.png/560px-Copyright-_all_rights_reserved.png',
   },
 ];
 
@@ -27,30 +28,37 @@ export const Footer: React.FC = memo(() => {
   }, []);
 
   return (
-    <footer className="footer">
-      <div className="footer__wrap">
-        <div className="footer__logo logo">
-          <img className="logo__img" src="../../stylesScss/images/FooterLogo.svg" alt="company logo" />
-        </div>
-        <nav className="footer__navFooter navFooter ">
-          <ul className="navFooter__list list">
-            {footerNav.map((item) => (
-              <li key={item.title} className="list__item">
-                <NavLink className="list__link" to={item.path}>{item.title}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="footer__lift lift">
-          <p className="lift__title">Back to top</p>
-          <button
-            type="button"
-            className="lift__button"
-            onClick={backToTop}
-          >
-            <img className="lift__buttonImg" src="../../stylesScss/images/FooterLogo.svg" alt="up arrow" />
-          </button>
-        </div>
+    <footer className="Footer">
+      <div className="Footer__left">
+        <Link to="/home">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="Footer__middle">
+        <ul className="Footer__list">
+          {footerLinks.map(({ link, url }) => (
+            <li
+              key={link}
+              className="Footer__item"
+            >
+              <a className="Footer__nav-link" href={url}>
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+      <div className="Footer__right">
+        <p className="Footer__backTo">Back to top</p>
+        <button
+          type="button"
+          className="Footer__backhome btn"
+          onClick={backToTop}
+        >
+          { }
+        </button>
+
       </div>
     </footer>
   );
