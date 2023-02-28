28590efe053911b9a85f17bbb85f472c2dcb8b6f
Andrii Petrinko <apetrinko@gmail.com>
2023-02-28 12:59:22 +0200

    added data files and rendering phones on the page

--- src/components/NewModelsList/NewModelsList.tsx
+++ src/components/NewModelsList/NewModelsList.tsx
@@ -1,192 +1,64 @@
 import React from 'react';
 import './NewModelsList.scss';
 import heart from '../../stylesScss/images/heart.svg';
+import phones from '../../data/phones.json';
 
 export const NewModelsList = () => {
+  const newModelsPhones = phones.sort((a, b) => b.year - a.year);
+
+  newModelsPhones.length = 4;
+
   return (
     <section>
       <h1 className="title">Brand new models</h1>
       <div className="catalog">
-        <div className="card">
-          <img
-            className="card__img"
-            src="https://techport.net.ua/image/cache/catalog/foto/8fc87a20d1926d27205d8f4da6ad15c9-700x700.jpg"
-            alt="iphone"
-          />
+        {newModelsPhones.map(el => (
+          <div className="card" key={el.id}>
 
-          <h2 className="card__name">
-            Apple iPhone 14 Pro 128GB Silver MQ023
-          </h2>
+            <img
+              src={el.image}
+              alt="iphone"
+              className="card__img"
+            />
+            <h2 className="card__name">
+              {el.name}
+            </h2>
 
-          <span className="card__price">$2,199</span>
+            <span className="card__price">{`${el.price}$`}</span>
 
-          <div className="card__underline"></div>
+            <div className="card__underline"></div>
 
-          <div className="card__description">
-            <span className="description__title">Screen</span>
-            <span className="description__value">6.1 Oled</span>
+            <div className="card__description">
+              <span className="description__title">Screen</span>
+              <span className="description__value">{el.screen}</span>
+            </div>
+            <div className="card__description">
+              <span className="description__title">Capacity</span>
+              <span className="description__value">{el.capacity}</span>
+            </div>
+            <div className="card__description">
+              <span className="description__title">RAM</span>
+              <span className="description__value">{el.ram}</span>
+            </div>
+
+            <div className="card__flex">
+              <a
+                href="/"
+                className="card__button"
+              >
+                Add to cart
+              </a>
+
+              <button
+                type="button"
+                className="card__add-to-favorite"
+              >
+                <img src={heart} alt="favorite" />
+              </button>
+            </div>
+
           </div>
-          <div className="card__description">
-            <span className="description__title">Capacity</span>
-            <span className="description__value">128Gb</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">RAM</span>
-            <span className="description__value">6 GB</span>
-          </div>
-
-          <div className="card__flex">
-            <a
-              href="/"
-              className="card__button"
-            >
-              Add to cart
-            </a>
-
-            <button
-              type="button"
-              className="card__add-to-favorite"
-            >
-              <img src={heart} alt="favorite" />
-            </button>
-          </div>
-
-        </div>
-        <div className="card">
-          <img
-            className="card__img"
-            src="https://techport.net.ua/image/cache/catalog/foto/8fc87a20d1926d27205d8f4da6ad15c9-700x700.jpg"
-            alt="iphone"
-          />
-
-          <h2 className="card__name">
-            Apple iPhone 14 Pro 128GB Silver MQ023
-          </h2>
-
-          <span className="card__price">$2,199</span>
-
-          <div className="card__underline"></div>
-
-          <div className="card__description">
-            <span className="description__title">Screen</span>
-            <span className="description__value">6.1 Oled</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">Capacity</span>
-            <span className="description__value">128Gb</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">RAM</span>
-            <span className="description__value">6 GB</span>
-          </div>
-
-          <div className="card__flex">
-            <a
-              href="/"
-              className="card__button"
-            >
-              Add to cart
-            </a>
-
-            <button
-              type="button"
-              className="card__add-to-favorite"
-            >
-              <img src={heart} alt="favorite" />
-            </button>
-          </div>
-
-        </div>
-        <div className="card">
-          <img
-            className="card__img"
-            src="https://techport.net.ua/image/cache/catalog/foto/8fc87a20d1926d27205d8f4da6ad15c9-700x700.jpg"
-            alt="iphone"
-          />
-
-          <h2 className="card__name">
-            Apple iPhone 14 Pro 128GB Silver MQ023
-          </h2>
-
-          <span className="card__price">$2,199</span>
-
-          <div className="card__underline"></div>
-
-          <div className="card__description">
-            <span className="description__title">Screen</span>
-            <span className="description__value">6.1 Oled</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">Capacity</span>
-            <span className="description__value">128Gb</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">RAM</span>
-            <span className="description__value">6 GB</span>
-          </div>
-
-          <div className="card__flex">
-            <a
-              href="/"
-              className="card__button"
-            >
-              Add to cart
-            </a>
-
-            <button
-              type="button"
-              className="card__add-to-favorite"
-            >
-              <img src={heart} alt="favorite" />
-            </button>
-          </div>
-
-        </div>
-        <div className="card">
-          <img
-            className="card__img"
-            src="https://techport.net.ua/image/cache/catalog/foto/8fc87a20d1926d27205d8f4da6ad15c9-700x700.jpg"
-            alt="iphone"
-          />
-
-          <h2 className="card__name">
-            Apple iPhone 14 Pro 128GB Silver MQ023
-          </h2>
-
-          <span className="card__price">$2,199</span>
-
-          <div className="card__underline"></div>
-
-          <div className="card__description">
-            <span className="description__title">Screen</span>
-            <span className="description__value">6.1 Oled</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">Capacity</span>
-            <span className="description__value">128Gb</span>
-          </div>
-          <div className="card__description">
-            <span className="description__title">RAM</span>
-            <span className="description__value">6 GB</span>
-          </div>
-
-          <div className="card__flex">
-            <a
-              href="/"
-              className="card__button"
-            >
-              Add to cart
-            </a>
-
-            <button
-              type="button"
-              className="card__add-to-favorite"
-            >
-              <img src={heart} alt="favorite" />
-            </button>
-          </div>
-
-        </div>
+        ))}
       </div>
     </section>
   );
