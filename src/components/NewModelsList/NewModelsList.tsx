a9d226e49774d98e922613d80db01b4a9189453a
nikachu404 <yasinskaa.veronika@gmail.com>
2023-03-01 11:14:14 +0200

    returned slider control

--- src/components/NewModelsList/NewModelsList.tsx
+++ src/components/NewModelsList/NewModelsList.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewModelsList.scss';
-// import heart from '../../stylesScss/images/heart.svg';
 import phones from '../../data/phones.json';
 import { PhoneCard } from '../PhoneCard/PhoneCard';
 
 export const NewModelsList = () => {
-  const newModelsPhones = phones.sort((a, b) => b.year - a.year);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  newModelsPhones.length = 4;
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === phones.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? phones.length - 1 : currentSlide - 1);
+  };
+
+  const slidesToShow = phones.sort((a, b) => b.year - a.year).slice(currentSlide, currentSlide + 4);
 
   return (
-    <section>
-      <h1 className="title">Brand new models</h1>
+    <section className="newModels">
+      <h1 className="newModels__title">Brand new models</h1>
+      <div className="newModels__slider-control">
+        <button type="button" className="slider-control__button" onClick={prevSlide}>
+          Prev
+        </button>
+        <button type="button" className="slider-control__button" onClick={nextSlide}>
+          Next
+        </button>
+      </div>
       <div className="catalog">
-        {newModelsPhones.map(el => (
+        {slidesToShow.map(el => (
           <PhoneCard phone={el} key={el.id} />
         ))}
       </div>
