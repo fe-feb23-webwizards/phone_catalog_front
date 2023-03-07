a3c0aca3235f0a2f50133ec020fffdf6347ea6bb
Andrii Petrinko <apetrinko@gmail.com>
2023-03-07 14:50:17 +0200

    added should show discount and fixed scroll for phones

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,10 +1,6 @@
 import { PhoneFromAPI } from '../types/PhoneFromAPI';
 import { client } from '../utils/fetchClients';
 
-export const getPhones = () => {
-  return client.get<PhoneFromAPI[]>('/phones');
-};
-
-export const getPhone = (page: number, perPage: number) => {
+export const getPhones = (page: number, perPage: number) => {
   return client.get<PhoneFromAPI[]>(`/phones?page=${page}&perPage=${perPage}`);
 };
