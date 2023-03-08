afc256706b7c7a761f4610dc18d0dee86eff53c0
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-03-08 12:47:02 +0200

    added grid to slider

--- src/components/Slider/TopSlider.tsx
+++ src/components/Slider/TopSlider.tsx
@@ -5,7 +5,9 @@ import React, {
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 
-import banner from './banner-desktop.jpeg';
+import banner_1 from './banner-desktop.jpeg';
+import banner_2 from '../../data/img/banner-phones.png';
+import banner_3 from '../../data/img/banner-tablets.png';
 
 import './TopSlider.scss';
 import '../../styles/_blocks/_gridTemplate.scss';
@@ -18,6 +20,10 @@ export const TopSlider: React.FC = memo(() => {
     slidesToShow: 1,
     slidesToScroll: 1,
     adaptiveHeight: true,
+    arrows: true,
+    draggable: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
     customPaging: () => (
       <div
         className="slick-dot"
@@ -27,9 +33,6 @@ export const TopSlider: React.FC = memo(() => {
         }}
       />
     ),
-    draggable: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 640,
@@ -43,30 +46,32 @@ export const TopSlider: React.FC = memo(() => {
   return (
     <section className="slider">
       <div className="slider__container">
-        <h1 className="slider__title">Welcome to Nice Gadgets store!</h1>
+        <div className="grid grid--tablet grid--desktop">
+          <h1 className="slider__title grid__item grid__item--tablet-1-9 grid__item--desktop-1-24">Welcome to Nice Gadgets store!</h1>
 
-        <Slider
-          className="grid__item"
-          {...settings}
-        >
-          <img
-            className="slider__image"
-            src={banner}
-            alt="banner for slider"
-          />
+          <Slider
+            className="grid__item grid__item--tablet-2-11 grid__item--desktop-2-23"
+            {...settings}
+          >
+            <img
+              className="slider__image"
+              src={banner_1}
+              alt="banner for slider"
+            />
 
-          <img
-            className="slider__image"
-            src={banner}
-            alt="banner for slider"
-          />
+            <img
+              className="slider__image"
+              src={banner_2}
+              alt="banner for slider"
+            />
 
-          <img
-            className="slider__image"
-            src={banner}
-            alt="banner for slider"
-          />
-        </Slider>
+            <img
+              className="slider__image"
+              src={banner_3}
+              alt="banner for slider"
+            />
+          </Slider>
+        </div>
       </div>
     </section>
   );
